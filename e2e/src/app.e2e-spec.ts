88358e175f0f786e0f80a65c8f39864b09cb6e4f
yanyu <ysoseriousc@gmail.com>
2019-04-10 14:20:43 +0900

    fix eslint bug

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to heros-demo!');
+    expect(page.getTitleText()).toEqual('Welcome to heroes-demo!');
   });
 
   afterEach(async () => {
